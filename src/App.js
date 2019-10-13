e31913a5abb15e171436e3c5f8663c92fb146b8e
German Nunez <germannunez@germans-mbp.home>
2019-10-12 19:59:32 -0700

    removed inital react files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Lyric App</h1>
     </div>
   );
 }
 
-export default App;
+export default App
