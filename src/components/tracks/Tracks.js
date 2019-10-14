3fa96abdf9493274cfc376ed2516a86555550fa5
German Nunez <germannunez@germans-mbp.home>
2019-10-13 19:41:10 -0700

    trying to get my commits to work

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -4,7 +4,7 @@ class Tracks extends Component {
     render() {
         return (
             <div>
-               <h1>Tracks</h1> 
+               <h1>Tracks</h1>
             </div>
         )
     }
