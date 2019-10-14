050df0704c7ef81d1f93c78d469a1a5780ad198f
German Nunez <germannunez@germans-mbp.home>
2019-10-13 20:55:59 -0700

    added provider component and dummy data to app js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Index from './components/layout/Index'
+import {Provider} from './context'
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <React.Fragment>
-      <Navbar/>
-      <div className="container">
-        <Switch>
-          <Route extact path ="/" render={()=> <Index/>}/>
-        </Switch>
-      </div>
-    </React.Fragment>
+    <Provider>
+      <React.Fragment>
+        <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route extact path ="/" render={()=> <Index/>}/>
+          </Switch>
+        </div>
+      </React.Fragment>
+    </Provider>
   );
 }
 
