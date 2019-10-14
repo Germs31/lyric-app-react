40ebfb1b4f3bb96d5cd6093ec394e8a919ec1166
German Nunez <germannunez@germans-mbp.home>
2019-10-13 19:24:12 -0700

    bootstrap added and navbar started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Landing from './components/Landing'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Lyric App</h1>
+      <Navbar/>
     </div>
   );
 }
