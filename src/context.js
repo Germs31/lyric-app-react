4c87b357baa7bc15f5a20d311d661dd12560a36d
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-10-14 17:18:40 -0700

    top ten tracks data rendering

--- src/context.js
+++ src/context.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 const Context = React.createContext()
 
 export class Provider extends Component {
     state = {
-        tracks_list: [
-            {track: {track_name: 'abc'}},
-            {track: {track_name: '123'}}
-        ],
+        tracks_list: [],
         heading:'Top Ten Tracks'
     }
+
+    componentDidMount() {
+        // const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}`)
+
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=US&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+            .then(res => {
+                console.log(res.data)
+                this.setState({tracks_list: res.data})
+            })
+            .catch(err => console.log(err))
+    }
+    
 
     render() {
         return (
