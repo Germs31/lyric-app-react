fc952c213ca7128be230f2a74d10de11488a06bf
German Nunez <germannunez@germans-mbp.home>
2019-10-14 19:14:26 -0700

    top ten lyrics render on screen

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Track =  (props) => {
     const { track } = props
@@ -7,6 +8,15 @@ const Track =  (props) => {
             <div className="card mb-4 shadow-sm">
                 <div className="card-body">
                     <h5>{track.artist_name}</h5>
+                    <p className="card-text">
+                        <strong><i className="fas fa-play"></i>Track</strong>: {track.track_name}
+                        <br/>
+                        <strong><i className="fas fa-compact-disc"></i>Album</strong>: {track.album_name}
+                        <br/>
+                    </p>
+                    <Link to={`lyrics/track/${track.track_id}`} className="btn btn-dark btn-block">
+                        <i className="fas fa-chevron-right"></i>View lyrics
+                    </Link>
                 </div>
             </div>
         </div>
